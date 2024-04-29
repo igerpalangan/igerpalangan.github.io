4a0a77927a11bdae5bd691d27a65fcb096c9c614
igerpalangan <s11820006@student.unklab.ac.id>
2024-04-29 08:42:07 +0800

    adding fie from local

---
+++ page/menuhome/home.js
@@ -0,0 +1,30 @@
+document.getElementById('konsuldocter').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../konsuldokter";
+    
+}
+document.getElementById('perubahanjadwal').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../perubahanjadwal";
+    
+}
+document.getElementById('perubahanjadwal').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../perubahanjadwal";
+    
+}
+document.getElementById('riwayatmedis').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../riwayatmedis";
+    
+}
+document.getElementById('formulirkesehatanku').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../formulirkesehatan/";
+    
+}
+document.getElementById('jadwaldokterku').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../jadwaldokter/";
+    
+}
